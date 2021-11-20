86c92fae977757a8ca98e279ec45bd628e644007
Woogys <apolontes@hanmail.net>
2021-11-20 17:39:11 +0900

    컨트롤러 분리

--- src/main/java/shop/kimkj/mytrip/controller/NearController.java
+++ src/main/java/shop/kimkj/mytrip/controller/NearController.java
@@ -1,5 +1,6 @@
 package shop.kimkj.mytrip.controller;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
 import org.json.XML;
 import org.springframework.beans.factory.annotation.Value;
@@ -18,13 +19,13 @@ import java.net.URL;
 
 @RestController
 @PropertySource("classpath:env.properties") // config.properties에 저장된 키 값 가져올 수 있게 경로 설정
-public class OpenApiController {
+public class NearController {
 
-    @Value("${KEY}")
-    private String KEY;
+    @Value("${TOUR_KEY}")
+    private String TOUR_KEY;
 
     @PostMapping("/near")
-    public String callApi(@RequestParam String lat_give, @RequestParam String lng_give) throws IOException {
+    public String getNearPlace(@RequestParam String lat_give, @RequestParam String lng_give) throws IOException {
         StringBuffer result = new StringBuffer();
         String jsonPrintString = null;
         try {
@@ -38,7 +39,7 @@ public class OpenApiController {
                     "&arrange=E" +
                     "&numOfRows=10" +
                     "&pageNo=1"
-                    , KEY, lng_give, lat_give);
+                    , TOUR_KEY, lng_give, lat_give);
 
             URL url = new URL(apiUrl);
 
@@ -62,4 +63,49 @@ public class OpenApiController {
         System.out.println(jsonPrintString);
         return jsonPrintString;
     }
+
+
+    @PostMapping("/near/place/intro")
+    public String getNearDetailIntro(@RequestParam(name = "content_id_give") String contentId) throws IOException {
+        StringBuffer result = new StringBuffer();
+        JSONObject jsonObject = null;
+        try {
+            String apiUrl = String.format(
+                    "http://api.visitkorea.or.kr/openapi/service/rest/KorService/detailCommon?" +
+                    "serviceKey=%s" +
+                    "&contentId=%s" +
+                    "&MobileOS=ETC" +
+                    "&MobileApp=TourAPI3.0_Guide" +
+                    "&defaultYN=Y&firstImageYN=Y" +
+                    "&areacodeYN=Y&catcodeYN=Y" +
+                    "&addrinfoYN=Y&mapinfoYN=Y" +
+                    "&overviewYN=Y&transGuideYN=Y"
+                    , TOUR_KEY, contentId);
+
+            URL url = new URL(apiUrl);
+
+            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.connect();
+
+            BufferedInputStream bufferedInputStream = new BufferedInputStream(urlConnection.getInputStream());
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(bufferedInputStream, "UTF-8"));
+
+            String returnLine;
+
+            while((returnLine = bufferedReader.readLine()) != null) {
+                result.append(returnLine);
+            }
+
+            jsonObject = XML.toJSONObject(result.toString());
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        JSONObject nearDetail = jsonObject.getJSONObject("response").getJSONObject("body").getJSONObject("items").getJSONObject("item");
+
+        return nearDetail.toString();
+    }
+
+
 }
