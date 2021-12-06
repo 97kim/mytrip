e851bfb1bf62dd2e1b7bad97423467a0e0deeb50
hyojin-dev <wkdgywls30@gmail.com>
2021-12-06 18:08:32 +0900

    업데이트

--- src/main/java/shop/kimkj/mytrip/dto/UserReviewRequestDto.java
+++ src/main/java/shop/kimkj/mytrip/dto/UserReviewRequestDto.java
@@ -1,9 +1,6 @@
 package shop.kimkj.mytrip.dto;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
-import org.springframework.web.multipart.MultipartFile;
-
 
 @Getter
 public class UserReviewRequestDto {
