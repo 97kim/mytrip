a13493ecbf9fa2cae1658f95cdd8281d419eaadb
97kim <rudwnd33@naver.com>
2021-12-08 10:32:42 +0900

    헬스체크 컨트롤러 추가

--- src/main/java/shop/kimkj/mytrip/controller/EbHealthCheck.java
+++ src/main/java/shop/kimkj/mytrip/controller/EbHealthCheck.java
@@ -2,13 +2,16 @@ package shop.kimkj.mytrip.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class EbHealthCheck {
 
     // EB 헬스체크를 위한 코드
-    @GetMapping(value = "/")
+    @GetMapping( "/")
     public ResponseEntity<?> doHealthCheck() {
-        return new ResponseEntity<>(HttpStatus.OK);
+        return ResponseEntity.ok("HealthCheck OK");
     }
 }
