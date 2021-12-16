3a77190a45a2efd2c2be0871bb2bf903aadc6474
Woogys <apolontes@hanmail.net>
2021-12-16 14:53:23 +0900

    추가 - Test code 작성을 위한 setter

--- src/main/java/shop/kimkj/mytrip/dto/LatLngDto.java
+++ src/main/java/shop/kimkj/mytrip/dto/LatLngDto.java
@@ -2,8 +2,10 @@ package shop.kimkj.mytrip.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
+import lombok.Setter;
 
 @Getter
+@Setter
 public class LatLngDto {
 
     @JsonProperty(value = "place_lat")
