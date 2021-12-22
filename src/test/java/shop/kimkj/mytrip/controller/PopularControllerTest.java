13b596ee150d2bc3df811f1f854632ff7a22c339
Woogys <apolontes@hanmail.net>
2021-12-23 00:10:00 +0900

    삭제 - 미사용 import문 삭제

--- src/test/java/shop/kimkj/mytrip/controller/PopularControllerTest.java
+++ src/test/java/shop/kimkj/mytrip/controller/PopularControllerTest.java
@@ -16,11 +16,9 @@ import org.springframework.web.filter.CharacterEncodingFilter;
 import shop.kimkj.mytrip.dto.LatLngDto;
 import shop.kimkj.mytrip.dto.PopularDto;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.forwardedUrlTemplate;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
