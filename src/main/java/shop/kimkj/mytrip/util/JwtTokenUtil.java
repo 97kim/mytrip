fd7222241550a779af0db119675ef1fe82d24416
hyojin-dev <wkdgywls30@gmail.com>
2021-12-28 12:42:18 +0900

    JwtTokenUtil 재적용

--- src/main/java/shop/kimkj/mytrip/util/JwtTokenUtil.java
+++ src/main/java/shop/kimkj/mytrip/util/JwtTokenUtil.java
@@ -14,6 +14,7 @@ import java.util.function.Function;
 
 @Component
 public class JwtTokenUtil implements Serializable {
+    private static final long serialVersionUID = -2550185165626007488L;
     public static final long JWT_TOKEN_VALIDITY = 5 * 60 * 60;
 
     @Value("${jwt.secret}")
